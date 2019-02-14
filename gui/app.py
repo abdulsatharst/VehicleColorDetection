e8475dfd8422af37fd18b669da73cee274c1ff98
abdulsatharst <abdulsatharst@gmail.com>
2019-02-14 19:45:46 +0530

    Add files via upload

---
+++ gui/app.py
@@ -0,0 +1,45 @@
+import sys
+from PyQt5.QtWidgets import QDialog, QApplication
+from hackathon_gui2 import Ui_Dialog
+import os
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog,QLineEdit,QPushButton
+
+class AppWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.ui.det_img_brw_btn.clicked.connect(self.open_file,self.button_click)
+        self.show()
+        self.le = QLineEdit()
+        self.le.setObjectName("det_img_txtbx")
+        self.le.setText("Host")
+
+
+    def open_file(self, main_window):
+        # Select the file dialog design.
+        dialog_style = QFileDialog.DontUseNativeDialog
+        dialog_style |= QFileDialog.DontUseCustomDirectoryIcons
+
+        # Open the file dialog to select an image file.
+        self.file_chosen, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "",
+                                                          "JPEG (*.JPEG *.jpeg *.JPG *.jpg *.JPE *.jpe *JFIF *.jfif);; PNG (*.PNG *.png);; GIF (*.GIF *.gif);; Bitmap Files (*.BMP *.bmp *.DIB *.dib);; TIFF (*.TIF *.tif *.TIFF *.tiff);; ICO (*.ICO *.ico)",
+                                                          options=dialog_style)
+
+        # Show the path of the file chosen.
+        if self.file_chosen:
+            main_window.det_img_txtbx.setText(self.file_chosen)
+            # Change the text on the label to display the file path chosen.
+        else:
+            main_window.det_img_txtbx.setText("No file was selected. Please select an image.")
+
+    def button_click(self):
+        # shost is a QString object
+        shost = self.le.setText()
+        print(shost)
+
+app = QApplication(sys.argv)
+w = AppWindow()
+w.show()
+sys.exit(app.exec_())
+
